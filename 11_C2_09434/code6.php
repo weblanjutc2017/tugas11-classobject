905b1ba3939d3f95dc904ade3220132ff9871ea4
Abdurrahman Trimanto <capteinn@gmail.com>
2017-09-30 01:00:01 +0700

    menambahkan file code6.php

---
+++ 11_C2_09434/code6.php
@@ -0,0 +1,18 @@
+<?php 
+class smartphone {
+   public $merk;
+   public $pemilik;
+ 
+   // static property
+   public static $harga_beli; 
+   //static method
+   public static function beli_smartphone() {
+     return "Beli smartphone seharga Rp".self::$harga_beli;
+   }
+}
+  
+// set static property
+smartphone::$harga_beli=3000000;
+// panggil static method
+echo smartphone::beli_smartphone();
+?>
