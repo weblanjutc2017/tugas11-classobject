c71c437cfc2ac847644a9db2b078413d5b46a90b
Abdurrahman Trimanto <capteinn@gmail.com>
2017-09-30 06:07:36 +0700

    menambahkan file code15.php

---
+++ 11_C2_09434/code15.php
@@ -0,0 +1,12 @@
+<?php 
+// buat abstract class
+abstract class smartphone{
+   public static function hidupkan_komputer(){
+     echo "Hidupkan Komputer";
+   }
+   // buat abstract method
+   abstract public function lihat_spec();
+   abstract public function lihat_processor();
+   abstract public function lihat_harddisk();
+   abstract public function lihat_pemilik();
+}
