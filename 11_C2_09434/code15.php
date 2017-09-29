3e149020b1042a69ec8bffd2df66a320a217762a
Abdurrahman Trimanto <capteinn@gmail.com>
2017-09-30 06:26:53 +0700

    solve problem output fungsi

--- 11_C2_09434/code15.php
+++ 11_C2_09434/code15.php
@@ -16,19 +16,27 @@ class tablet extends smartphone{
 		return parent::hidupkan_komputer();
 	}
 	public function lihat_spec(){
-		
+		echo "Lihat spec tablet...";
 	}
 	public function lihat_processor(){
-		
+		echo "Lihat processor tablet...";
 	}
 	public function lihat_harddisk(){
-		
+		echo  "Lihat hardisk tablet...";
 	}
 	public function lihat_pemilik(){
-		
+		echo "Pemilik tablet adalah...";
 	}
 } 
 
 $tablet_baru = new tablet();
 $tablet_baru->hidupkan_komputer();
+echo "<br>";
+$tablet_baru->lihat_spec();
+echo "<br>";
+$tablet_baru->lihat_processor();
+echo "<br>";
+$tablet_baru->lihat_harddisk();
+echo "<br>";
+$tablet_baru->lihat_pemilik();
 ?>
