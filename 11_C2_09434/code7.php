2758bb20056dc948f74e8d92f92017b93da41296
Abdurrahman Trimanto <capteinn@gmail.com>
2017-09-30 01:07:20 +0700

    solve menambahkan method beli_semua

--- 11_C2_09434/code7.php
+++ 11_C2_09434/code7.php
@@ -5,12 +5,19 @@ class smartphone {
    }
 } 
 class smartphone_dualsim extends smartphone{
+   protected static function beli_smartphone() {
+	   return parent::beli_smartphone();
+   }
    private static function beli_smartphone_dualsim(){
      return "Beli smartphone dual sim baru";
    }
+   
   
    public static function beli_semua(){
      //lengkapi baris kode berikut ini
+	 echo parent::beli_smartphone();
+	 echo "<br>";
+	 echo self::beli_smartphone_dualsim();
    }
 }
   
